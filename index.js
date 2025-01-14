76a92d60d4e15e1bd5ff343f3bfde95dac5e9080
paula rcv <paula-vetter@web.de>
2025-01-14 19:41:46 +0000

    file metadata microservice

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var cors = require('cors');
+var multer = require("multer");
 require('dotenv').config()
 
 var app = express();
@@ -7,9 +8,27 @@ var app = express();
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+// Set up multer for file handling
+var storage = multer.memoryStorage();
+var upload = multer({storage: storage});
+
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+
+// endpoint for file upload
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  if (!req.file) {
+    return res.status(400).json({error: 'No file uploaded'});
+  }
+
+  // respond with file metadata
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+})
 
 
 
